44569b325b078d6b3b6dfa3e960e45d4491dd13a
VVS <vvs.narayana@bigbasket.com>
2021-11-17 23:17:58 +0530

    BB-93160: timeouts

--- iam/iam.go
+++ iam/iam.go
@@ -206,7 +206,7 @@ func NewClient(baseARN string, regional bool, stsVpcEndPoint string) (*Client, e
 		config = config.WithEndpointResolver(client)
 	}
 	config = config.WithHTTPClient(&http.Client{
-		Timeout: 1 * time.Second,
+		Timeout: 2 * time.Second,
 	})
 	client.StsService = sts.New(sess, config)
 

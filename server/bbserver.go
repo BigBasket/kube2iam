1e672e542ce6de54d575580f8c3087cbbbbd9300
VVS <vvs.narayana@bigbasket.com>
2021-12-14 20:16:18 +0530

    BB-93160: added pprof

--- server/bbserver.go
+++ server/bbserver.go
@@ -3,10 +3,24 @@ package server
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
+
+	"net/http/pprof"
 )
 
 func startServer(server *Server) {
 	echoInstance := echo.New()
+
+	echoInstance.Any("/debug/pprof/trace", func(context echo.Context) error {
+		pprof.Trace(context.Response().Writer, context.Request())
+
+		return nil
+	})
+
+	echoInstance.Any("/debug/pprof/", func(context echo.Context) error {
+		pprof.Index(context.Response().Writer, context.Request())
+
+		return nil
+	})
 
 	echoInstance.Any("/:version/meta-data/iam/security-credentials", server.getInstanceRole)
 	echoInstance.Any("/:version/meta-data/iam/security-credentials/", server.getInstanceRole)

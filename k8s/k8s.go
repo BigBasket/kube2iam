b8a01da30ade8c49d10747e98c5e4f5475b4567d
satya256 <vvs.narayana@bigbasket.com>
2023-08-10 15:15:44 +0530

    BB-171210: typo

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -135,7 +135,6 @@ func getPodFromK8s(k8s *Client, IP string) (*v1.Pod, error) {
 // If the indexed pods all have HostNetwork = true the function return nil and the error message.
 // If we retrive a running pod that doesn't have HostNetwork = true and it is in Running state will return that.
 func resolveDuplicatedIP(k8s *Client, IP string) (*v1.Pod, error) {
-	k8s.CoreV1().Pods("").Get()
 	runningPodList, err := k8s.CoreV1().Pods("").List(metav1.ListOptions{
 		FieldSelector: selector.OneTermEqualSelector("status.podIP", IP).String(),
 	})
